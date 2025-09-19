fc514cf746f4223580bde37b98a23422a94e9e69
ModderG <ginerprojects@gmail.com>
2025-09-19 20:20:01 -0300

    ⚫ Rounder Blocks & Minimalistic Logo

--- js/projects.js
+++ js/projects.js
@@ -11,16 +11,17 @@ const projects = [
 
 
 document.getElementById("projects-container").innerHTML = projects.map(p => `
-<a href="${p.link}" class="p-5 base-secondary-hoverable relative">
+<a href="${p.link}" class="p-5 base-secondary-hoverable relative rounded-lg">
   <div class="flex items-center gap-4 mb-10">
     <img src="${p.img}" alt="${p.title}" style="width: ${p.w}rem; height: auto;">
-    <span class="font-bold truncate leading-normal ${p.color}" style="font-size: 4vh;">
+
+    <span class="font-bold truncate leading-normal ${p.color} mt-5" style="font-size: 3.5vh;">
       ${p.title}
     </span>
   </div>
 
-  <p class="absolute left-5 bottom-5 truncate line-clamp-2" style="font-size: 2vh;">
-    ${p.desc}
+  <p class="absolute left-0 bottom-5 truncate leading-normal w-full px-5" style="font-size: 1.8vh;">
+      ${p.desc}
   </p>
 </a>
 
