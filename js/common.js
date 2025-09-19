6fb6348d9e38839956cd639173171cbe01332e44
ModderG <ginerprojects@gmail.com>
2025-09-19 17:07:38 -0300

    🎨 Improvind The Design a Bit

--- js/common.js
+++ js/common.js
@@ -1,7 +1,7 @@
 const navbarHTML = `
-<nav class="base-secondary-section top-section bg-secondary-selected">
+<nav class="bg-secondary-selected top-section bg-secondary-selected" >
   <h1 class="font-bold" style="font-size: 4vh;">
-    <a href="index.html" class="hover:underline">G's Lore</a>
+      <a href="index.html" class="hover:underline hover:text-special">G's Lore</a>
   </h1>
 
   <div class="flex space-x-4 items-center">
@@ -32,7 +32,7 @@ const navbarHTML = `
 `;
 
 const footerHTML = `
-<footer class="text-center text-sm text-gray-400 mt-10 p-10 base-secondary-section mt-auto">
+<footer class="text-center text-sm text-secondary-selected p-10 bg-secondary mt-auto">
   © 2025 <a href="#" class="underline hover:text-white">CodderG</a>
 </footer>
 `;
--- js/projects.js
+++ js/projects.js
@@ -1,20 +1,20 @@
 const projects = [
   { title: "Tameable Beasts", desc: "Mc mod that adds tameable mobs.", link: "tb_lore.html", 
-    img: "./assets/images/tb_logo.png", color:"tb-text", w:3},
+    img: "./assets/images/tb_logo.png", color:"tb-text", w:2.5},
   
-  { title: "The Digimod", desc: "Mc mod that adds digital creatures for you to rise.", link: "#", 
-    img: "./assets/images/td_logo.png", color:"td-text", w:8},
+  { title: "The Digimod", desc: "Mc mod that adds digital creatures to raise.", link: "#", 
+    img: "./assets/images/td_logo.png", color:"td-text", w:6},
   
-  { title: "Model Portfolio", desc: "Compilation of the best Mc assets made by me.", link: "portfolio.html", 
-    img: "./assets/images/p_logo.png", color:"p-text", w:5}
+  { title: "Model Portfolio", desc: "Compilation of my best Mc assets.", link: "portfolio.html", 
+    img: "./assets/images/p_logo.png", color:"p-text", w:4}
 ];
 
 
 document.getElementById("projects-container").innerHTML = projects.map(p => `
-  <a href="${p.link}" class="block rounded p-10 base-secondary-hoverable">
+  <a href="${p.link}" class="p-10 base-secondary-hoverable">
   <div class="flex items-center gap-4">
       <img src="${p.img}" alt="${p.title}" style="width: ${p.w}rem; height: auto;">
-    <span class="font-bold truncate leading-normal ${p.color}" style="font-size: 5vh;">${p.title}</span>
+    <span class="font-bold truncate leading-normal ${p.color}" style="font-size: 4vh;">${p.title}</span>
   </div>
   <p class="truncate mt-6 line-clamp-2" style="font-size: 2vh;">${p.desc}</p>
 </a>
