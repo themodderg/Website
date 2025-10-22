28c74273137c69b54c76184d3025ace14dc938eb
CodderG <127430228+ginerJ@users.noreply.github.com>
2025-10-22 10:30:26 -0300

    👾 First Steps Of The Digimod Wiki & Design Changes

--- js/texts/the_digimod.js
+++ js/texts/the_digimod.js
@@ -6,3 +6,32 @@ document.getElementById("td-lore").innerHTML = `
         <span class="text-orange">train</span> and <span class="text-orange">fight</span> with.
     </p>
   `;
+
+document.getElementById("faq-join-world-lore").innerHTML = `
+  <p class="font-bold" style="font-size: 2vh;">
+    <span class="text-special">FIRST TIME JOINING A WORLD</span>: You will be given your first random <span class="text-orange">baby digimon</span> partner along with 
+    a random <span class="text-orange">digivice</span>, training gear and digimeat (Digivices don't actually have different functionalities, just pure aesthetics). 
+    In case you somehow lost the baby digimon item (which is explosion and burning proof) you can get more from <span class="text-orange">Digimon Nest</span>, scattered around the world.
+    You can also get more digimon by defeating wild digimon with your partner, as a rare drop.
+  </p>
+`;
+
+document.getElementById("faq-mistakes").innerHTML = `
+  <p class="font-bold" style="font-size: 2vh;">
+    <span class="text-special">CARE MISTAKES & LIVES</span>: You may lock yourself out of some <span class="text-orange">evolutions</span> if you commit a <span class="text-orange">care 
+    mistake</span>, be a good tamer! <span class="text-orange">Care mistakes</span> 
+    happen if you train with a <span class="text-orange">sad</span> digimon until it becomes <span class="text-orange">depressed</span> or if you lose a <span class="text-orange">life</span> in battle. 
+    Your digimon has <span class="text-orange">3 lives</span>, 
+    which can be lost if your digimon <span class="text-orange">dies</span> like a regular mob. You can restore a live by using a <span class="text-orange">DigiCore</span> item. Mistakes are unremovable
+    unless you <span class="text-orange">devolve</span> your digimon. 
+  </p>
+`;
+
+document.getElementById("faq-diet").innerHTML = `
+  <p class="font-bold" style="font-size: 2vh;">
+    <span class="text-special">WHAT DO I FEED MY DIGIMON?</span>: Not every digimon eats the same. In order to <span class="text-orange">improve the mood</span> of your digimon 
+    (which gets lowered via training), you will have to <span class="text-orange">feed</span> them their preferred foods. You can check their <span class="text-orange">diets</span> in the 
+    <span class="text-orange">Digivice GUI</span> which they will be ranked from the most preferred to the least. Specifically <span class="text-orange">DigiMeat</span> is common in most diets, 
+    which is an item that can be grown as a <span class="text-orange">crop</span>.
+  </p>
+`;
