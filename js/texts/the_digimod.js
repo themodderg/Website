693aad63bf25aa01b0a3e5145c504d97bf7dfe0d
ModderG <ginerprojects@gmail.com>
2025-12-14 21:00:07 -0300

    Updated The Digimod Wiki

--- js/texts/the_digimod.js
+++ js/texts/the_digimod.js
@@ -2,36 +2,80 @@ document.getElementById("td-lore").innerHTML = `
     <p class="font-bold" style="font-size: 2vh;">
       <span class="td-text">The Digimod</span> is a Minecraft mod that adds Minecraft style modeled
        <span class="text-special">Digimon</span>
-       to your world which you will have to <span class="text-orange">rise</span>,
+       to your world which you will be able to <span class="text-orange">rise</span>,
         <span class="text-orange">train</span> and <span class="text-orange">fight</span> with.
     </p>
   `;
 
 document.getElementById("faq-join-world-lore").innerHTML = `
   <p class="font-bold" style="font-size: 2vh;">
-    <span class="text-special">FIRST TIME JOINING A WORLD</span>: You will be given your first random <span class="text-orange">baby digimon</span> partner along with 
-    a random <span class="text-orange">digivice</span>, training gear and digimeat (Digivices don't actually have different functionalities, just pure aesthetics). 
-    In case you somehow lost the baby digimon item (which is explosion and burning proof) you can get more from <span class="text-orange">Digimon Nest</span>, scattered around the world.
-    You can also get more digimon by defeating wild digimon with your partner, as a rare drop.
+    <span class="text-special">WHAT TO DO WHEN JOINING A WORLD?</span>: You will be given your first random <span class="text-orange">baby digimon</span> partner along with 
+    a random <span class="text-orange">digivice</span>, <span class="text-orange">training gear</span> and <span class="text-orange">digimeat</span>
+     (Digivices don't actually have different functionalities, just pure aesthetics). 
+    In case you somehow lost the baby digimon item (which is explosion and burning proof) you can get more from 
+    <span class="text-orange">Digimon Nests</span>, scattered around the world.
+    You can also get more <span class="text-orange">Baby I</span> digimon by defeating wild digimon with your partner, 
+    as a <span class="text-orange">rare drop</span>. It is suggested to start a Digimeat farm in order to keep your digimon healthy and 
+    on a well mood.
+  </p>
+`;
+
+document.getElementById("faq-diet").innerHTML = `
+  <p class="font-bold" style="font-size: 2vh;">
+    <span class="text-special">WHAT DO I FEED MY DIGIMON?</span>: You <span class="text-orange">want</span> to feed your digimon 
+    <span class="text-orange">constantly</span>, in order to <span class="text-orange">heal</span> it, <span class="text-orange">avoid</span> care mistakes and keep it on
+    a <span class="text-orange">well mood</span>, but not every digimon eats the same. You will have to <span class="text-orange">feed</span> them their preferred foods. You can check their <span class="text-orange">diets</span> in the 
+    <span class="text-orange">Digivice GUI</span> which will be ranked from the most preferred to the least. Specifically <span class="text-orange">DigiMeat</span> is common in most diets, 
+    which, as stated before is an item that can be grown as a <span class="text-orange">crop</span>.
   </p>
 `;
 
 document.getElementById("faq-mistakes").innerHTML = `
   <p class="font-bold" style="font-size: 2vh;">
-    <span class="text-special">CARE MISTAKES & LIVES</span>: You may lock yourself out of some <span class="text-orange">evolutions</span> if you commit a <span class="text-orange">care 
-    mistake</span>, be a good tamer! <span class="text-orange">Care mistakes</span> 
-    happen if you train with a <span class="text-orange">sad</span> digimon until it becomes <span class="text-orange">depressed</span> or if you lose a <span class="text-orange">life</span> in battle. 
+    <span class="text-special">HOW DO LIFES & CARE MISTAKES WORK?</span>: You may lock yourself out of some <span class="text-orange">evolutions</span> 
+    if you commit enough <span class="text-orange">care 
+    mistakes</span>, so be a good tamer! <span class="text-orange">Care mistakes</span> 
+    happen if you train with a <span class="text-orange">sad</span> digimon until it becomes <span class="text-orange">depressed</span> or if you 
+    lose a <span class="text-orange">life</span> in battle or get your digimon too <span class="text-orange">dirty</span>. 
     Your digimon has <span class="text-orange">3 lives</span>, 
     which can be lost if your digimon <span class="text-orange">dies</span> like a regular mob. You can restore a live by using a <span class="text-orange">DigiCore</span> item. Mistakes are unremovable
     unless you <span class="text-orange">devolve</span> your digimon. 
   </p>
 `;
 
-document.getElementById("faq-diet").innerHTML = `
+document.getElementById("faq-poop").innerHTML = `
   <p class="font-bold" style="font-size: 2vh;">
-    <span class="text-special">WHAT DO I FEED MY DIGIMON?</span>: Not every digimon eats the same. In order to <span class="text-orange">improve the mood</span> of your digimon 
-    (which gets lowered via training), you will have to <span class="text-orange">feed</span> them their preferred foods. You can check their <span class="text-orange">diets</span> in the 
-    <span class="text-orange">Digivice GUI</span> which they will be ranked from the most preferred to the least. Specifically <span class="text-orange">DigiMeat</span> is common in most diets, 
-    which is an item that can be grown as a <span class="text-orange">crop</span>.
+    <span class="text-special">WHY IS MY DIGIMON PURPLE?</span>: Obviously, digimon poop after eating and get 
+    <span class="text-orange">dirty</span>. Dirty digimon will look <span class="text-orange">purple</span>
+     and have a dirty status added to their mood, 
+    you can <span class="text-orange">clean</span> them with <span class="text-orange">paper</span> or <span class="text-orange">water</span>, 
+    <span class="text-orange">very dirty</span> digimon will gain <span class="text-orange">care mistakes</span>. 
+    
   </p>
 `;
+
+document.getElementById("evo-line").innerHTML = `
+  <p class="font-bold" style="font-size: 2vh;">
+    <span class="text-special">GETTING YOUR DIGIMON TO EVOLVE</span>: <span class="text-orange">Level up</span> your digimon by having it
+    <span class="text-orange">defeat</span> other digimon in combat. 
+    <span class="text-orange">Babies I</span> evolve to Baby II after some time, while <span class="text-orange">Baby II</span>
+    to Rookie evolution will happen at level 6, 
+    <span class="text-orange">Rookie</span> to Champion at level 20, and <span class="text-orange">Champion</span> to Perfect at level 40.
+  </p>
+`;
+
+document.getElementById("evo-conds").innerHTML = `
+  <p class="font-bold" style="font-size: 2vh;">
+    <span class="text-special">EVOLVING INTO THE CORRECT DIGIMON</span>: Digimon have multiple <span class="text-orange">evolution routes</span>,
+     you have to <span class="text-orange">meet</span> the specific <span class="text-orange">conditions</span> the evolution you want requires, 
+     <span class="text-orange">otherwise</span> the evolution you will get will be a <span class="text-orange">random roll</span> of the ones you meet the requirements for.<br>
+    <br>
+    Digimon evolution routes are affected by:<br>
+    - Amount of <span class="text-orange">battles won</span>.<br>
+    - Amount of <span class="text-orange">care mistakes</span> made.<br>
+    - Current <span class="text-orange">mood</span>.<br>
+    - Type of the <span class="text-orange">experience</span> won in combat.<br>
+    - <span class="text-orange">Items</span> that are being kept on the digimon inventory.<br>
+  </p>
+`;
+
