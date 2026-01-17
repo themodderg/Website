6d656ccfc203808883f8b97799d3dd77e8aa7d8b
ModderG <ginerprojects@gmail.com>
2026-01-17 11:14:12 -0300

    Added Digimod [Cobblemon] Wiki support

--- js/projects.js
+++ js/projects.js
@@ -1,21 +1,30 @@
 const projects = [
   { title: "Tameable Beasts", desc: "Mc mod that adds new tameable mobs to the world.", link: "tb_lore.html", 
-    img: "./assets/images/tb_logo.png", color:"tb-text", w:2},
+    img: "./assets/images/tb_logo.png", color:"tb-text"},
   
-  { title: "The Digimod", desc: "Mc mod that adds digital creatures to raise to the world.", link: "td_lore.html", 
-    img: "./assets/images/td_logo.png", color:"td-text", w:4.5},
+  { title: "The Digimod", desc: "Mc mod that ports the digimon franchise to the world.", link: "td_lore.html", 
+    img: "./assets/images/td_logo.png", color:"td-text"},
   
   { title: "Model Portfolio", desc: "Compilation of my best 3D Blockbench assets.", link: "portfolio.html", 
-    img: "./assets/images/p_logo.png", color:"p-text", w:3},
+    img: "./assets/images/p_logo.png", color:"p-text"},
+  
+  { title: "Companions!", desc: "Mc mod that adds creative tameable mobs and a boss fight.", link: "https://www.curseforge.com/minecraft/mc-mods/companions-mod", 
+    img: "./assets/images/co_logo.png", color:"co-text"},
+  
+  { title: "Digimod [Cobblemon]", desc: "Mc addon that brings Digimon into the Cobblemon mod.", link: "txc_lore.html", 
+    img: "./assets/images/txc_logo.png", color:"txc-text"},
+  
+  { title: "P.E.K.K.A. Craft", desc: "Mc mod that adds tameable P.E.K.K.A.s", link: "https://www.curseforge.com/minecraft/mc-mods/pk-craft", 
+    img: "./assets/images/pk_logo.png", color:"pk-text"},
+  
+  { title: "Animights", desc: "Mc mod that adds anthropomorphic loyal squires.", link: "https://www.curseforge.com/minecraft/mc-mods/animights", 
+    img: "./assets/images/am_logo.png", color:"am-text"},
   
   { title: "????", desc: "??????<br>??????", link: "index.html", 
-    img: "./assets/images/unknown_logo.png", color:"td-text", w:3},
+    img: "./assets/images/unknown_logo.png", color:"td-text"},
   
   { title: "????", desc: "??????<br>??????", link: "index.html", 
-    img: "./assets/images/unknown_logo2.png", color:"td-text", w:3},
-  
-  { title: "????", desc: "??????<br>??????", link: "index.html", 
-    img: "./assets/images/unknown_logo3.png", color:"td-text", w:3}
+    img: "./assets/images/unknown_logo3.png", color:"td-text"}
 ];
   
 
---
+++ js/texts/digimod_cobblemon.js
@@ -0,0 +1,7 @@
+document.getElementById("txc-lore").innerHTML = `
+    <p class="font-bold" style="font-size: 2vh;">
+      <span class="txc-text">Digimod [Cobblemon]</span> is a <span class="text-special">Cobblemon</span> addon that
+       adds Minecraft style modeled <span class="text-special">Digimon</span>
+       to your world in replacement of some <span class="text-special">Pokemon</span>.
+    </p>
+  `;
