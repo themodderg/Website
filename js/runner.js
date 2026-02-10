1a43ce9fcc56a0b45ba48234a0cf712406e51226
ModderG <ginerprojects@gmail.com>
2026-02-10 14:43:51 -0300

    Added Walkers

---
+++ js/runner.js
@@ -0,0 +1,30 @@
+const runners = [
+    'teddy.gif',
+    'racoon.gif',
+    'mini_pk.gif',
+    'sword.gif',
+    'cactus_golem.gif',
+    'agumon.gif'
+];
+
+function spawnRunner() {
+    const container = document.querySelector('.hero-text');
+    if (!container) return;
+
+    const randomRunner = runners[Math.floor(Math.random() * runners.length)];
+
+    const img = document.createElement('img');
+    img.src = `assets/images/runners/${randomRunner}`;
+    img.classList.add('hero-runner');
+    img.alt = 'Runner Animation';
+
+    container.appendChild(img);
+
+    img.addEventListener('animationend', () => {
+        img.remove();
+    }, { once: true });
+}
+
+setTimeout(spawnRunner, 2000);
+
+setInterval(spawnRunner, 20000);
