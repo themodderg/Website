771d2525739565304cd3a1a789ec754f2cd7c44a
ModderG <ginerprojects@gmail.com>
2026-03-16 23:57:57 -0300

    Added PEKKA Craft 4.0 lore

--- js/texts/pk_craft.js
+++ js/texts/pk_craft.js
@@ -7,13 +7,21 @@ document.getElementById("pk-lore").innerHTML = `
 
 document.getElementById("pancakes-lore").innerHTML = `
     <p class="font-bold" style="font-size: 2vh;">
-      Craft a <span class="td-text">P.E.K.K.A. Helmet</span> out of valuable material which you can either equip as gear equivalent to 
+      Craft a <span class="td-text">Mini P.E.K.K.A. Helmet</span> out of valuable material which you can either equip as gear equivalent to 
       a <span class="text-special">Netherite Helmet</span> or use it to summon a <span class="text-special">Mini P.E.K.K.A.</span> when 
       placing it on top of an <span class="text-special">Iron Block</span> and interacting with a <span class="text-special">Pancake</span>. 
       The summoned mob will drop it's head to be rebuilt after dying and will get healed after eating some <span class="text-special">Pancakes</span>.
     </p>
   `;
 
+document.getElementById("butterfly-lore").innerHTML = `
+    <p class="font-bold" style="font-size: 2vh;">
+      Craft a <span class="td-text">P.E.K.K.A. Helmet</span> out of valuable material which you can either equip as gear equivalent to 
+      a <span class="text-special">Netherite Helmet</span> or use it to summon the <span class="text-special">P.E.K.K.A.</span> when 
+      placing it on top of a cross made of <span class="text-special">Iron Blocks</span>. 
+      The summoned mob will drop it's head to be rebuilt after dying and will get passively healed after eating some time.
+    </p>
+  `;
 
 document.getElementById("poty-lore").innerHTML = `
     <p class="font-bold" style="font-size: 2vh;">
@@ -29,5 +37,25 @@ document.getElementById("easter-lore").innerHTML = `
     <p class="font-bold" style="font-size: 2vh;">
         Get a new texture on you <span class="text-special">Mini P.E.K.K.A.</span> by giving it the "pancakes" name using a 
         <span class="text-special">Name Tag</span>, this is only a visual effect and provides no stat changes. 
+    </p>
+  `;
+
+document.getElementById("hero-lore").innerHTML = `
+    <p class="font-bold" style="font-size: 2vh;">
+        <span class="text-special">Mini P.E.K.K.A.s</span> can be turned in their <span class="text-special">Star Level</span> version after defeating 50 foes and the transformation gets triggered by crafting a <span class="text-special">Star</span> and using it on the <span class="text-special">Mini P.E.K.K.A.</span> A <span class="text-special">Star Level P.E.K.K.A.</span> can be turned into the <span class="text-special">Hero P.E.K.K.A.</span> by crafting and using an <span class="text-special">Hero Crystal</span> over it.
+    </p>
+    <br>
+    <p class="font-bold" style="font-size: 2vh;">
+        This transformed mob will charge it's <span class="text-special">Ability Bar</span> with each hit, once full a player can interact with it with a <span class="text-special">Pancake</span> to unleash a <span class="text-special">Rage</span> like mode where it will get healed and recieve a damage boost for 30 seconds.
+    </p>
+  `;
+
+document.getElementById("evo-lore").innerHTML = `
+    <p class="font-bold" style="font-size: 2vh;">
+        <span class="text-special">P.E.K.K.A.s</span> can be turned in their <span class="text-special">Star Level</span> version after defeating 50 foes and the transformation gets triggered by crafting a <span class="text-special">Star</span> and using it on the <span class="text-special">P.E.K.K.A.</span> A <span class="text-special">Star Level P.E.K.K.A.</span> can be turned into the <span class="text-special">Evolved P.E.K.K.A.</span> by crafting and using an <span class="text-special">Evo Crystal</span> over it.
+    </p>
+    <br>
+    <p class="font-bold" style="font-size: 2vh;">
+        This evolved mob will spawn <span class="text-special">Butterflies</span> after defeating its foes which will heal itself upon being consumed.
     </p>
   `;
