73ba6fe47e0bb233211bcfc50d9a0442d0aee757
ModderG <ginerprojects@gmail.com>
2026-03-17 11:42:38 -0300

    Pekkas on the portfolio

--- js/portfolio_pics.js
+++ js/portfolio_pics.js
@@ -1,37 +1,38 @@
 const pics = [
-  {img: "allay_mage.png"},
-  {img: "antlion.png"},
-  {img: "chikote.png"},
-  {img: "cornelius_croak.png"},
-  {img: "croissant_dragon.png"},
-  {img: "cultist.png"},
-  {img: "illager_golem.png"},
-  {img: "lib.png"},
-  {img: "minions.png"},
-  {img: "penguin.png"},
-  {img: "pinocchi.png"},
-  {img: "pontiff.png"},
-  {img: "racoon.png"},
-  {img: "sword_shade.png"},
-  {img: "quadruped_shade.png"},
-  {img: "roly_poly.png"},
-  {img: "teddy.png"},
-  {img: "shiny_beetle.png"},
-  {img: "ground_beetle.png"},
-  {img: "quetzal.png"},
-  {img: "scarecrow.png"},
-  {img: "graptera.png"},
-  {img: "argentavis.png"},
-  {img: "minipk.png"},
-  {img: "cactus_golem.png"},
-  {img: "gold_golem.png"},
-  {img: "castle_golem.png"}
+    { img: "allay_mage.png" },
+    { img: "antlion.png" },
+    { img: "chikote.png" },
+    { img: "cornelius_croak.png" },
+    { img: "croissant_dragon.png" },
+    { img: "cultist.png" },
+    { img: "illager_golem.png" },
+    { img: "lib.png" },
+    { img: "minions.png" },
+    { img: "penguin.png" },
+    { img: "pinocchi.png" },
+    { img: "pontiff.png" },
+    { img: "racoon.png" },
+    { img: "sword_shade.png" },
+    { img: "quadruped_shade.png" },
+    { img: "roly_poly.png" },
+    { img: "teddy.png" },
+    { img: "shiny_beetle.png" },
+    { img: "ground_beetle.png" },
+    { img: "quetzal.png" },
+    { img: "scarecrow.png" },
+    { img: "graptera.png" },
+    { img: "argentavis.png" },
+    { img: "minipk.png" },
+    { img: "pekka.png" },
+    { img: "cactus_golem.png" },
+    { img: "gold_golem.png" },
+    { img: "castle_golem.png" }
 ];
 
 function renderPics() {
-    const cols = window.innerWidth < window.innerHeight? 2:4;
+    const cols = window.innerWidth < window.innerHeight ? 2 : 4;
     const gap = 11;
-    const size = Math.floor((window.innerWidth - (cols-1) * gap) / cols);
+    const size = Math.floor((window.innerWidth - (cols - 1) * gap) / cols);
 
     document.getElementById("pics-container").innerHTML = pics.map(p => `
     <div class="inline-block rounded w-fit">
